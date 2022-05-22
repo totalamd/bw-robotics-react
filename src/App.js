1614c927f6753e061ee3e002f7985e824a788c1f
totalamd <totalamd@users.noreply.github.com>
2022-05-22 11:54:28 +0300

    fix page title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "./styles.css"; // Styles should be divided by component in a big project
 
 import Main from "./Main";
@@ -10,7 +10,12 @@ export default function App() {
     name: "O'coffee Brazil",
     dataUrl: "http://127.0.0.1:8080"
   });
-  const [mode, setMode] = useState({ type: "vehicles", name: "Vehicles" });  // different Views
+
+  const [mode, setMode] = useState({ type: "vehicles", name: "Vehicles" });  // for different Views in Main component
+
+  useEffect(() => {
+    document.title = `${mode.name} - ${customer.name}`;
+  });
 
   return (
     <div className="App">
