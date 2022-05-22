1be5771b04b648a3b895a9b2a70ed83e1033cadc
totalamd <totalamd@users.noreply.github.com>
2022-05-22 11:41:31 +0300

    fix menu icon

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import "./styles.css";
+import menuIcon from "./img/menu-black-48.png";
 
 export default function Menu() {
   const [showMenu, setMenu] = useState(false);
@@ -7,12 +8,12 @@ export default function Menu() {
   let menuItems = menuItemsArray.map(item => <div className="Menu__Item" key={item}>{item}</div>);
 
   function handleClick() {
-    setMenu(showMenu => !showMenu);
+    setMenu(status => !status);
   }
 
   return (
-    <div className="Menu" onClick={handleClick}>
-      [M]
+    <div className="Menu">
+      <div className="Menu__Start" onClick={handleClick}></div>
       {showMenu && <div className="Menu__DropDown">{menuItems}</div>}
     </div> // need to catch mouseclicks out of menu to close
   );
