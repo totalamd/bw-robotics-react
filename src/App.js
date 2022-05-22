fe23265838d919dea799b4c1764780f688b3a750
totalamd <totalamd@users.noreply.github.com>
2022-05-22 08:27:27 +0300

    img folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import "./styles.css";
+import "./styles.css"; // Styles should be divided by component in a big project
 
 import Main from "./Main";
 import Navbar from "./Navbar";
