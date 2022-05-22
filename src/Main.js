1de67e8e7e7f0ab91dea49dfb4210aba0e5da997
totalamd <totalamd@users.noreply.github.com>
2022-05-22 12:16:33 +0300

    refactor Main

--- src/Main.js
+++ src/Main.js
@@ -3,19 +3,18 @@ import "./styles.css";
 import Map from "./Map";
 
 export default function Main(props) {
-  let view;
-  switch (props.mode) {
-    case 'vehicles':
-      view = <Map dataSource={props.dataSource} />;
-      break;
-
-    default:
-      break;
+  let view = () => {
+    switch (props.mode) {
+      case 'vehicles':
+        return <Map dataSource={props.dataSource} />;
+      default:
+        break;
+    }
   }
 
   return ( // switch rendering component according to props
     <div className="Main">
-      {view}
+      {view()}
     </div>
   );
 }
